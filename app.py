64107743cb7e39ec72ac72a17bf1f2163bb1b4c1
Anil Nair <anilnairxyz@gmail.com>
2019-04-30 19:06:25 +0100

    added xorwin library

--- app.py
+++ app.py
@@ -1,12 +1,8 @@
 from flask import Flask
+from apis import api
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+api.init_app(app)
 
 if __name__ == '__main__':
     app.run()
